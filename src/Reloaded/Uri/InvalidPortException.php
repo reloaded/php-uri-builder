e72d7c6879e8abb45fd262f7ee6da26906fce64e
elitereloaded <1337reloaded@gmail.com>
2015-06-18 11:48:33 -0700

    Add InvalidPortException

---
+++ src/Reloaded/Uri/InvalidPortException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @author Reloaded <1337reloaded@gmail.com>
+ * @since 6/18/15 11:47 AM
+ * @version 1.0.0
+ */
+
+namespace Reloaded\Uri;
+
+
+/**
+ * Exception thrown if the URI port is invalid.
+ *
+ * Class InvalidPortException
+ * @package Reloaded\Uri
+ */
+class InvalidPortException extends \Exception
+{
+
+}
