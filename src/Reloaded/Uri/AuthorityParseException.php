714a56421a687e5bb4efb3bffca77cfb0295b269
elitereloaded <JL4^Yj@?CY8jio>
2015-06-18 22:17:37 -0700

    Add AuthorityParseException

---
+++ src/Reloaded/Uri/AuthorityParseException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Reloaded <1337reloaded@gmail.com>
+ * @since 6/18/2015 10:17 PM
+ * @version 1.0.0
+ */
+
+
+namespace Reloaded\Uri;
+
+
+/**
+ * Exception thrown if there was an error parsing the URI authority.
+ *
+ * Class AuthorityParseException
+ * @package Reloaded\Uri
+ */
+class AuthorityParseException 
+{
+
+}
