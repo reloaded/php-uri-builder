b9cea2a9d2f0644f41f1ce77c94dfa601e960f45
elitereloaded <JL4^Yj@?CY8jio>
2015-06-27 03:31:30 -0700

    Add getReservedChars() and getUnreservedChars()

--- src/Reloaded/Uri/AbstractBuilder.php
+++ src/Reloaded/Uri/AbstractBuilder.php
@@ -491,5 +491,53 @@ namespace Reloaded\Uri
 
             return $this;
         }
+
+        /**
+         * Returns an array of reserved characters that need to be percent encoded. Array keys are the raw characters
+         * and array values are the percent-encoded representation of the array key character.
+         *
+         * @return string[]
+         */
+        public function getReservedChars()
+        {
+            return [
+                "!" => "%20",
+                "$" => "%24",
+                "&" => "%26",
+                "'" => "%27",
+                "(" => "%28",
+                ")" => "%29",
+                "*" => "%2A",
+                "+" => "%2B",
+                "," => "%2C",
+                ";" => "%3B",
+                "=" => "%3D",
+                ":" => "%3A",
+                "/" => "2F",
+                "?" => "%3F",
+                "#" => "%23",
+                "[" => "%5B",
+                "]" => "%5D",
+                "@" => "%40"
+            ];
+        }
+
+        /**
+         * Returns an array of unreserved characters that do not need to be percent-encoded. Array keys are the raw characters
+         * and array values are the percent-encoded representation of the array key character.
+         *
+         * This array does not contain a list of DIGITS or ALPHA characters.
+         *
+         * @return string[]
+         */
+        public function getUnreservedChars()
+        {
+            return [
+                "-" => "%2D",
+                "." => "%2E",
+                "_" => "%5F",
+                "~" => "%7E"
+            ];
+        }
     }
 }
