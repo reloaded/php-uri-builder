2d6de661ee316f5fc992b8882af4f303d2499573
elitereloaded <JL4^Yj@?CY8jio>
2015-06-27 03:38:58 -0700

    Move CharacterTrait into namespace Reloaded\Uri

--- src/Reloaded/Uri/CharacterTrait.php
+++ src/Reloaded/Uri/CharacterTrait.php
@@ -6,58 +6,61 @@
  */
 
 
-/**
- * Provides APIs for manipulating reserved and unreserved characters when generating URIs.
- *
- * Class CharacterTrait
- */
-trait CharacterTrait 
+namespace Reloaded\Uri
 {
     /**
-     * Returns an array of reserved characters that need to be percent encoded. Array keys are the raw characters
-     * and array values are the percent-encoded representation of the array key character.
+     * Provides APIs for manipulating reserved and unreserved characters when generating URIs.
      *
-     * @return string[]
+     * Class CharacterTrait
      */
-    public function getReservedChars()
+    trait CharacterTrait
     {
-        return [
-            "!" => "%20",
-            "$" => "%24",
-            "&" => "%26",
-            "'" => "%27",
-            "(" => "%28",
-            ")" => "%29",
-            "*" => "%2A",
-            "+" => "%2B",
-            "," => "%2C",
-            ";" => "%3B",
-            "=" => "%3D",
-            ":" => "%3A",
-            "/" => "2F",
-            "?" => "%3F",
-            "#" => "%23",
-            "[" => "%5B",
-            "]" => "%5D",
-            "@" => "%40"
-        ];
-    }
+        /**
+         * Returns an array of reserved characters that need to be percent encoded. Array keys are the raw characters
+         * and array values are the percent-encoded representation of the array key character.
+         *
+         * @return string[]
+         */
+        public function getReservedChars()
+        {
+            return [
+                "!" => "%20",
+                "$" => "%24",
+                "&" => "%26",
+                "'" => "%27",
+                "(" => "%28",
+                ")" => "%29",
+                "*" => "%2A",
+                "+" => "%2B",
+                "," => "%2C",
+                ";" => "%3B",
+                "=" => "%3D",
+                ":" => "%3A",
+                "/" => "2F",
+                "?" => "%3F",
+                "#" => "%23",
+                "[" => "%5B",
+                "]" => "%5D",
+                "@" => "%40"
+            ];
+        }
 
-    /**
-     * Returns an array of unreserved characters that do not need to be percent-encoded. Array keys are the raw characters
-     * and array values are the percent-encoded representation of the array key character.
-     *
-     * This array does not contain a list of DIGITS or ALPHA characters.
-     *
-     * @return string[]
-     */
-    public function getUnreservedChars()
-    {
-        return [
-            "-" => "%2D",
-            "." => "%2E",
-            "_" => "%5F",
-            "~" => "%7E"
-        ];
+        /**
+         * Returns an array of unreserved characters that do not need to be percent-encoded. Array keys are the raw characters
+         * and array values are the percent-encoded representation of the array key character.
+         *
+         * This array does not contain a list of DIGITS or ALPHA characters.
+         *
+         * @return string[]
+         */
+        public function getUnreservedChars()
+        {
+            return [
+                "-" => "%2D",
+                "." => "%2E",
+                "_" => "%5F",
+                "~" => "%7E"
+            ];
+        }
     }
 }
