adcf3bd61b441cba37aa2f2bee359d0ca895ad0b
elitereloaded <JL4^Yj@?CY8jio>
2015-06-27 02:20:27 -0700

    Add InvalidQueryException

---
+++ src/Reloaded/Uri/InvalidQueryException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Reloaded <1337reloaded@gmail.com>
+ * @since 6/27/2015 2:19 AM
+ * @version 1.0.0
+ */
+
+
+namespace Reloaded\Uri;
+
+
+/**
+ * Exception thrown if a URI query parameter is invalid.
+ *
+ * Class InvalidQueryException
+ * @package Reloaded\Uri
+ */
+class InvalidQueryException extends \Exception
+{
+
+}
