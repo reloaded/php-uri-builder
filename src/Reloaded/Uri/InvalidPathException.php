1e723e82158925045192ece065ed5af41e28936d
elitereloaded <JL4^Yj@?CY8jio>
2015-07-15 23:41:17 -0700

    Add InvalidPathException

---
+++ src/Reloaded/Uri/InvalidPathException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Reloaded <1337reloaded@gmail.com>
+ * @since 7/15/2015 11:40 PM
+ * @version 1.0.0
+ */
+
+
+namespace Reloaded\Uri;
+
+
+/**
+ * Exception thrown if the path is invalid.
+ *
+ * Class InvalidPathException
+ * @package Reloaded\Uri
+ */
+class InvalidPathException 
+{
+
+}
