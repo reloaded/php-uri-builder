f590042606fc4b91ae9f778109914c6b8316e7a6
elitereloaded <JL4^Yj@?CY8jio>
2015-07-16 00:42:44 -0700

    Add InvalidFragmentException

---
+++ src/Reloaded/Uri/InvalidFragmentException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Reloaded <1337reloaded@gmail.com>
+ * @since 7/16/2015 12:42 AM
+ * @version 1.0.0
+ */
+
+
+namespace Reloaded\Uri;
+
+
+/**
+ * Thrown if URI fragment is invalid.
+ *
+ * Class InvalidFragmentException
+ * @package Reloaded\Uri
+ */
+class InvalidFragmentException extends \Exception
+{
+
+}
