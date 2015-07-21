df624e5ffd0011478130adcdf58b312bcd937276
elitereloaded <1337reloaded@gmail.com>
2015-07-20 17:08:18 -0700

    Add Builder unit test

---
+++ tests/unit/BuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class BuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Reloaded\Uri\Builder
+     */
+    private $stub;
+
+    protected function setUp()
+    {
+        $this->stub = new \Reloaded\Uri\Builder();
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testToString()
+    {
+
+    }
+
+}
