0e8ba1f0b7ffb87a058b4d4d9fb07b61b1d2626a
reloaded <1337reloaded@gmail.com>
2016-04-20 00:13:16 -0700

    Try and get travis ci builds to pass

--- tests.cmd
+++ tests.cmd
@@ -1,1 +1,2 @@
+call vendor\bin\codecept build -c codeception.yml -f
 call vendor\bin\codecept run -c codeception.yml -f
--- tests.sh
+++ tests.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+./vendor/bin/codecept build -c codeception.yml -f
 ./vendor/bin/codecept run -c codeception.yml -f
